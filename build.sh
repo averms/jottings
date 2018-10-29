5f568f00d0c861fcf076192cbbdd0665c6aa320f
Aman <amanraoverma@gmail.com>
2018-10-29 11:00:25 -0400

    Copy only the html file, css is uneccesary

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sass jottings.scss:dist/jottings.css
-cp jottings.* ~/.pandoc/templates/
+cp jottings.html ~/.pandoc/templates/
 pandoc --template=jottings.html --section-divs --toc -o "test/test.html" "test/test.md"
