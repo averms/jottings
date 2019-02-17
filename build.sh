8a4b728e1c95b68bfa23f827b1c8a193f0de3649
Aman <amanraoverma@gmail.com>
2019-02-17 15:36:57 -0500

    Improve build script.

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 cd test || exit 1
+testdir="$(pwd)"
 sassc -l -t compact ../jottings.scss jottings.css
+
 # send to my data dir
-sed 's|{{jottings}}|/Users/a3/dv/jottings/test/jottings.css|' ../jottings.html \
-    > "$HOME/.pandoc/templates/jottings.html"
+sed "s|{{jottings}}|${testdir}/jottings.css|" ../jottings.html \
+    >"$HOME/.pandoc/templates/jottings.html"
+
 # send to test for testing
 sed 's|{{jottings}}|jottings.css|' ../jottings.html \
-    > "jottings.temp"
+    >jottings.temp
+
 pandoc --template=jottings.temp --toc -o "test.html" "test.md"
 cp -R ./ ../docs
